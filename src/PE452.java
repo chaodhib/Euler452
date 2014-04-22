196559ceb1c736d30986dc505f155d70c1d8a222
Chaouki <chaodhib@gmail.com>
2014-04-22 16:19:57 +0200

    Preparation en vue de changements en vue de vérif de théorie mathématique. Changements à retrograder ensuite

--- src/PE452.java
+++ src/PE452.java
@@ -1,10 +1,12 @@
 import java.math.BigInteger;
 import com.google.common.math.BigIntegerMath;
+import org.apache.commons.math3.special.Gamma;
 
 
-public class PE452 {
+public class PE452 // cmd profiler : jvisualvm
+{
 	static int m=10, n=m; // max so far :  15 et 15 en 22s
-	static BigInteger compteur;
+	static BigInteger compteur, compteur_d;
 	
 	static int k[];
 	
@@ -13,7 +15,7 @@ public class PE452 {
 //		int k[]={0,0,0,2,0,0,2,0,0,0,0,2,0,0,9};
 //		System.out.println(doPart2(k));
 		
-		while(true)
+//		while(true)
 		{
 			k=new int[m];
 			findSolution2();
@@ -25,24 +27,35 @@ public class PE452 {
 	
 	static void findSolution2()
 	{
-		compteur=BigInteger.ZERO;
+		compteur=BigInteger.ZERO; compteur_d=BigInteger.ZERO;
 		long debut= System.nanoTime();
-		doPartX(0, 1, m);
+		doPartX(0, 1, 0);
 		long fin= System.nanoTime();
 		System.out.print("Pour m: "+m+" et n: "+n);
 		System.out.print(" La solution au problème est: "+compteur+" e"+(compteur.toString().length()-1));
 		System.out.println(" et le calcul a pris: "+ (fin-debut)/1000000 +"ms");
+		System.out.println("compteur_d: "+compteur_d);
+		System.out.println("Arrangments: "+calculArrangmentsAvecR());
+	}
+	
+	static long calculArrangmentsAvecR()
+	{
+		long resultat=1;
+		for(int k=0;k!=n+1;k++)
+			resultat*=n+k+1;
+		
+		return resultat;
 	}
 	
 	static void doPartX(int sum0, int prod0, int d)
 	{
-		if(d==0)
+		if(d==m)
 		{
-			//if(doPart2(k) && sum0==n)
 			if(sum0==n)
 			{
-//				show(k); System.out.println();
 				compteur=compteur.add(doPart3v2(k));
+				compteur_d=compteur_d.add(BigInteger.ONE);
+				show(k);
 			}
 				
 			return;
@@ -57,20 +70,29 @@ public class PE452 {
 				if(sum>n)
 					return;
 				if(k!=0)
-					prod*=m-d+1;
+					prod*=d+1;
 				if(prod>m)
 					return;
-				PE452.k[m-d]=k;
-				doPartX(sum, (int)prod, d-1);
+				PE452.k[d]=k;
+				doPartX(sum, (int)prod, d+1);
 			}
 		}
+	}
+	
+	static BigInteger doPart3(int k[]) // calcul le nbre de n-uples associés au vecteur k 
+	{
+		BigInteger answer=BigIntegerMath.factorial(n);
+		for(int i=0; i<m;i++)
+			answer=answer.divide(BigIntegerMath.factorial(k[i]));
+		
+		return answer;
 	}
 	
 	static BigInteger doPart3v2(int k[]) // calcul le nbre de n-uples associés au vecteur k 
 	{
 		// recherche de la valeur max dans k
 		int max=k[0], ind=0;
-		for(int i=1; max<n/2 && i<k.length; i++)
+		for(int i=1; max<n/2 && i<k.length; i++) //TODO au lieu de recalculer n/2 à chaque tour de boucle, le faire qu'une fois et pour /2, utiliser le décallement d'un cran vers la droite
 		{
 			if(k[i]>max)
 			{
@@ -96,10 +118,20 @@ public class PE452 {
 		return answer;
 	}
 	
+	static BigInteger doPart3v3(int k[]) //pas précis (nécessite tweaks sur la précision) et pas particulierement efficasement utile
+	{
+		double answer=Gamma.logGamma(n+1);
+		for(int i=0;i<m;i++)
+			answer-=Gamma.logGamma(k[i]+1);
+		
+		return BigInteger.valueOf((long)java.lang.Math.exp(answer));
+	}
+	
 	static void show(int[] k) 
 	{
 		for(int i=0; i<k.length ; i++)
 			System.out.print("|"+k[i]+"|");
+		System.out.println();
 	}
 
 	static BigInteger factorial(int n2)	// n2 >=0  

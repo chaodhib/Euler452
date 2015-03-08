d66c4f8aa924a58711e99f061197bdb720f98016
Chaouki Dhib <chaodhib@gmail.com>
2015-03-08 19:08:59 +0100

    Converted the project into a Maven project for its simple dependency management. I also added the algorithm that generates the partitions.

---
+++ src/be/chaouki/eulerproblem/tests/Test14.java
@@ -0,0 +1,50 @@
+package be.chaouki.eulerproblem.tests;
+
+import java.util.Arrays;
+
+/**modified version of the algorithm to generate the partition of N found here:
+ * http://introcs.cs.princeton.edu/java/23recursion/Partition.java.html
+ * 
+ * This version uses recursivity. Therefore its probably not very efficient for high values of N.
+ * But it doesnt matter here because we only need to get the partition of values up to 29 (case where n=m=10^9).
+ * 
+ * @author Chaouki
+ *
+ */
+public class Test14 {
+	
+	private final static int N=7, I=100;
+
+	public static void main(String[] args) {
+		long debut = System.nanoTime();
+		partition(N);
+		partitionWithOffset(N, 1);
+		long fin = System.nanoTime();
+		System.out.println("le calcul a pris: " + (fin - debut) / 1000000+ "ms");
+	}
+	
+	public static void partition(int n) {
+		int vect[]=new int[n];
+        partition(n, n, vect, 0);
+    }
+	
+	public static void partitionWithOffset(int n, int offset) {
+		int vect[]=new int[n+1];
+		vect[0]=I-n;
+        partition(n, n, vect, offset);
+    }
+	
+    private static void partition(int n, int max, int vect[], int ind) {
+        if (n == 0) {
+            System.out.println(Arrays.toString(vect)); //Treatment on the vector should be placed here
+            return;
+        }
+  
+        for (int i = Math.min(n, max); i >= 1; i--) {
+        	vect[ind]=i;
+            partition(n-i, i, vect, ind+1);
+            vect[ind]=0;
+        }
+    }
+
+}
--- src/be/chaouki/eulerproblem/tests/Test3.java
+++ src/be/chaouki/eulerproblem/tests/Test3.java
@@ -1,16 +1,16 @@
 package be.chaouki.eulerproblem.tests;
-import com.objectwave.utility.CombinatoricException;
+//import com.objectwave.utility.CombinatoricException;
 
 
 public class Test3 {
 
-	public static void main(String[] args) throws CombinatoricException {
-		int n=1000;
-		Integer ints[]=new Integer[n];
-		for(int i=0; i<n ; i++)
-			ints[i]=i+1;
-		MultiCombinations mc=new MultiCombinations(ints, n);
-
-	}
+//	public static void main(String[] args) throws CombinatoricException {
+//		int n=1000;
+//		Integer ints[]=new Integer[n];
+//		for(int i=0; i<n ; i++)
+//			ints[i]=i+1;
+//		MultiCombinations mc=new MultiCombinations(ints, n);
+//
+//	}
 
 }
